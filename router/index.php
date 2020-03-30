c90e4b3ab2785d31b1d9ded9bfe996b5ec8a27ea
exchangezak <exchangezak1@gmail.com>
2020-03-30 21:45:40 +0200

    popup

---
+++ router/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+<h1>popup</h1>
+<div id="container">
+<div id="model">
+    <button id="close">close</button>
+</div>
+</div>
+
+
+
+
+
+
+
+
+    <script src="assets/js/app.js"></script>
+</body>
+</html>
