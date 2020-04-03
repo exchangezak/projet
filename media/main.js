ee47eb10f9a8d9c177a1fb529809837b6994360c
exchangezak <exchangezak1@gmail.com>
2020-04-03 12:40:16 +0200

    yep

---
+++ media/main.js
@@ -0,0 +1,17 @@
+
+fetch("http://newsapi.org/v2/everything?q=bitcoin&from=2020-03-03&sortBy=publishedAt&apiKey=d8bb7e6e132c4bacbc3758246de2c819")
+https://newsapi.org/v2/top-headlines?country=us&apiKey=d8bb7e6e132c4bacbc3758246de2c819
+// .then(function(res) {
+//   return res.json();
+// })
+// .then(function(data) {
+  
+//     const reslength = data.articles.length;
+//     const randomIndex = Math.floor(Math.random() * reslength);
+//     const auteur = data.articles[randomIndex].author;
+//     const titre = data.articles[randomIndex].title;
+//     const description = data.articles[randomIndex].description;
+//   console.log(auteur)
+//   console.log(titre)
+//   console.log(description)
+// })
